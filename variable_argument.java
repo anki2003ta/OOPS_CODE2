1bac22f002ef37cae7a2abd73c00ac48b6764b7b
Ankita Nath <145554790+anki2003ta@users.noreply.github.com>
2024-12-11 17:46:16 +0530

    Create variable_argument.java

---
+++ variable_argument.java
@@ -0,0 +1,19 @@
+/*variable argument*/
+public class Test
+{
+    static int func(int ...v)
+    {
+        int sum=0;
+        for(int n:v)
+        {
+            sum=sum+n;
+        }
+        return sum;
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("func(1,2,3)"+func(1,2,3));
+        System.out.println("func(1,2)"+func(1,2));
+        System.out.println("func(1,2,3,4,5)"+func(1,2,3,4,5));
+    }
+}
